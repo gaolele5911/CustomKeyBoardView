542ef79284b3e7caf1aa69e301711025c73dbbef
gaolele <gaolele@navinfo.com>
2018-10-24 15:42:26 +0800

    Initial Commit

---
+++ CustomKeyBoardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomKeyBoardView
+//
+//  Created by gaol on 2018/10/24.
+//  Copyright © 2018 gaol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomKeyBoardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomKeyBoardView
+//
+//  Created by gaol on 2018/10/24.
+//  Copyright © 2018 gaol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
